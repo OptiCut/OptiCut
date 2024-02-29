62ac07885338ac2bd7503297f68b98f2c7e80fb1
clamb11 <123419865+clamb11@users.noreply.github.com>
2024-02-29 11:43:47 -0700

    Update OptiCut.py

--- OptiCut/OptiCut.py
+++ OptiCut/OptiCut.py
@@ -1,10 +1,15 @@
 # Assuming you have not changed the general structure of the template no modification is needed in this file.
 from . import commands
 from .lib import fusion360utils as futil
+import adsk.core
 
 
 def run(context):
     try:
+        if not context['IsApplicationStartup']:
+            app = adsk.core.Application.get()
+            ui = app.userInterface
+            ui.messageBox("OptiCut add-in has been loaded and has added a new \"OptiCut\" to the ADD-INS panel in the UTILITIES tab of the DESIGN workspace", "OptiCut")
         # This will run the start function in each of your commands as defined in commands/__init__.py
         commands.start()
 
